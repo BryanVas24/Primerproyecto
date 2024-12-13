5ad8905326f9b3f544f1393da13ab777c9d4fa86
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-13 15:34:54 -0600

    mostrando un usuario dinamicamente

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+//Esto es el decorador e indica como se construye el component
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  //Esto es como una prop y puede ser publica o privada, si es privada solo se usa en la clase
+  selectedUser = DUMMY_USERS[randomUserIndex];
+}
