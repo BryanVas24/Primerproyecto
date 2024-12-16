ac573b45295af444dc703b0120bd2f333679792b
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-16 16:09:08 -0600

    Usando output()

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 //Esto es el decorador e indica como se construye el component
 @Component({
@@ -15,7 +15,9 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
-  @Output() select = new EventEmitter();
+  //El output sirve para pasar info de un hijo a un padre
+  //@Output() select = new EventEmitter();
+  select = output<string>();
   get getImagePath() {
     return `assets/users/${this.avatar}`;
   }
