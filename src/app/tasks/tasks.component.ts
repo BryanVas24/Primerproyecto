4176b63cc3418b4144af1cfd361c5879bf205ccd
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-18 11:36:31 -0600

    Mostrando tareas dinamicamente pero aún no se muestra el contenido

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,4 +10,43 @@ import { TaskComponent } from './task/task.component';
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Dominar Angular',
+      summary:
+        'Aprender todas las características básicas y avanzadas de Angular y cómo aplicarlas.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Crear el primer prototipo',
+      summary: 'Crear un primer prototipo del sitio web de la tienda en línea',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Preparar plantilla de problemas',
+      summary:
+        'Preparar y describir una plantilla de problemas que ayudará con la gestión del proyecto',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't4',
+      userId: 'u2',
+      title: 'Probar aplicación',
+      summary:
+        'Debes probar la aplicación que esta siendo construida en este proyecto',
+      dueDate: '2025-12-31',
+    },
+  ];
+
+  get selectedUsertasks() {
+    return this.tasks.filter((task) => task.userId === this.id);
+  }
 }
