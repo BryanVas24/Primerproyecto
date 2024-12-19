14d262b39e7faaf23b26cb1623855f35bc404866
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-19 16:57:54 -0600

    usando ng-content, el equivalante a un Outlet o un children parece ser

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 //Esto es el decorador e indica como se construye el component
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
