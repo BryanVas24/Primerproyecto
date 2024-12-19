c4600f67adefbdaefc6e7572104ae43db850803d
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-19 16:03:52 -0600

    Usando ngModel y FormsModule para toma de datos de formulario

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -12,4 +13,8 @@ export class NewTaskComponent {
   closeModalbtn() {
     this.closeModal.emit();
   }
+  //Datos del formulario
+  title = '';
+  summary = '';
+  dueDate = '';
 }
