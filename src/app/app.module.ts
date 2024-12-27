469865447b7c6ed429afb0c8beea715193404a20
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2024-12-27 17:11:58 -0600

    Usando componentes standalone con componentes basados en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 @NgModule({
-  declarations: [],
+  //Acá van todos los componentes a utilizar
+  declarations: [AppComponent],
+  //Componente raíz que es con el que inicia la app
+  bootstrap: [AppComponent],
+  //Para importar componentes que son standalone
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppModule {}
